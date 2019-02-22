ce8c64d656f61656e51eb344289409c64e14acbf
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-02-22 12:38:44 +0100

    implemented methods from Sop in ShopImpl class without exceptions

--- src/main/java/com/codecool/ShopImpl.java
+++ src/main/java/com/codecool/ShopImpl.java
@@ -1,5 +1,6 @@
 package com.codecool;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,8 +10,6 @@ public class ShopImpl implements Shop {
     private String name;
     private String owner;
     private boolean isOpen = true;
-    private List<Product> productList;
-
     private Map<Long, ShopImpl.ShopEntryImpl> products;
 
     public ShopImpl(String name, String owner) {
@@ -45,14 +44,27 @@ public class ShopImpl implements Shop {
 
     @Override
     public List<Product> getProducts() {
-        return this.productList;
+
+        List<Product> productList = new ArrayList<>();
+
+        for(long barcode : products.keySet()) {
+            if(products.get(barcode).getQuantity() > 1){
+                for(int i = 0; i < products.get(barcode).getQuantity(); i++) {
+                    productList.add(products.get(barcode).getProduct());
+                }
+            }else if(products.get(barcode).getQuantity() == 1) {
+                productList.add(products.get(barcode).getProduct());
+            }
+        }
+        return productList;
     }
 
     @Override
     public Product findByName(String name) {
-        for(int i = 0; i < productList.size(); i++) {
-            if(productList.get(i).getName().equals(name)) {
-                return productList.get(i);
+
+        for(long barcode : products.keySet()) {
+            if(products.get(barcode).getProduct().getName().equals(name)) {
+                return products.get(barcode).getProduct();
             }
         }
         return null;
@@ -60,12 +72,51 @@ public class ShopImpl implements Shop {
 
     @Override
     public boolean hasProduct(long barcode) {
-        return false;
+
+        if(products.get(barcode).getQuantity() > 0) {
+            return true;
+        }else
+            return false;
     }
 
     @Override
     public float getPrice(long barcode) {
+        for(long key : products.keySet()) {
+            if(key == barcode) {
+                return products.get(key).getPrice();
+            }
+        }
+        return 0;
+    }
 
+    @Override
+    public void addNewProduct(Product product, int quantity, float price) {
+
+        products.put(product.getBarcode(), new ShopEntryImpl(product, quantity, price));
+    }
+
+    @Override
+    public void addProduct(long barcode, int quantity) {
+
+        products.get(barcode).increaseQuantity(quantity);
+    }
+
+    @Override
+    public Product buyProduct(long barcode) {
+
+        products.get(barcode).decreaseQuantity(1);
+        return products.get(barcode).getProduct();
+    }
+
+    @Override
+    public List<Product> buyProducts(long barcode, int quantity) {
+
+        List<Product> buyList = new ArrayList<>();
+
+        for(int i = 0; i < quantity; i++) {
+            buyList.add(products.get(barcode).getProduct());
+        }
+        return buyList;
     }
 
     private class ShopEntryImpl {
@@ -105,11 +156,11 @@ public class ShopImpl implements Shop {
         }
 
         public void increaseQuantity(int amount) {
-
+            this.quantity = this.quantity + amount;
         }
 
         public void decreaseQuantity(int amount) {
-
+            this.quantity = this.quantity - amount;
         }
     }
 }
